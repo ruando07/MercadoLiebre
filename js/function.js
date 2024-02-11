7dddaf7de4d72403540097a4fb20f13d36b90b2a
ruando07 <ruando1997@gmail.com>
2024-02-11 11:30:32 -0500

    cambios

---
+++ js/function.js
@@ -0,0 +1,14 @@
+// Codigo para mostrar el año y la fecha en el footer
+const currentYear = new Date().getFullYear();
+const yearElement = document.getElementById("currentYear");
+yearElement.textContent = currentYear;
+
+
+var str1 = "apple";
+var str2 = "banana";
+console.log(str1.localeCompare(str2)); 
+
+
+
+
+
